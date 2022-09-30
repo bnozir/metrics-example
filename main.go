627f849d9bd366679f286e1687c4d2bb4c8935e6
bnozir <bnozir@macbookpro-nozir.local>
2022-09-30 23:04:42 +0300

    Was added requests couter for custom handlers /hello and /bue

--- main.go
+++ main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	fmt.Println("Hi!")
 
+	handlerCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "metrics_example_handlers_requests_total",
+		Help: "RPC latency distributions.",
+	},
+		[]string{"method", "handler", "code"},
+	)
+	prometheus.MustRegister(handlerCounter)
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		handlerCounter.With(prometheus.Labels{
+			"method":  r.Method,
+			"handler": r.RequestURI,
+			"code":    "200",
+		}).Inc()
+
 		fmt.Fprintf(w, "Hello every one!")
 	})
+
 	http.HandleFunc("/bue", func(w http.ResponseWriter, r *http.Request) {
+		handlerCounter.With(prometheus.Labels{
+			"method":  r.Method,
+			"handler": r.RequestURI,
+			"code":    "200",
+		}).Inc()
+
 		fmt.Fprintf(w, "Bue every one! See you soon!")
 	})
+
 	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":1234", nil)
