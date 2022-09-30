1abd04bcecbbbdb3d30501d9d52556979fca55cd
bnozir <bnozir@macbookpro-nozir.local>
2022-09-30 22:39:34 +0300

    Was added a default Prometheus metrics about system

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
-func main()  {
+func main() {
 	fmt.Println("Hi!")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello every one!")
+	})
+	http.HandleFunc("/bue", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bue every one! See you soon!")
+	})
+	http.Handle("/metrics", promhttp.Handler())
+
+	http.ListenAndServe(":1234", nil)
 }
